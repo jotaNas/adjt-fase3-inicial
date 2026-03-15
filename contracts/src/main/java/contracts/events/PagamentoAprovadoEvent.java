b76bda83fa5393c414428b5473b9d1d844143ffc
João Nascimento <69321552+jotaNas@users.noreply.github.com>
2026-03-15 16:45:17 -0300

    Remove unused fields from PagamentoAprovadoEvent
    Removed unused fields 'x' and 'a' from PagamentoAprovadoEvent.

--- contracts/src/main/java/contracts/events/PagamentoAprovadoEvent.java
+++ contracts/src/main/java/contracts/events/PagamentoAprovadoEvent.java
@@ -8,8 +8,6 @@ public record PagamentoAprovadoEvent(
         UUID pedidoId,
         UUID clienteId,
         int valor,
-        Boolean x,
-        BigInteger a,
         Instant ocorridoEm
 ) implements IntegrationEvent {
 
